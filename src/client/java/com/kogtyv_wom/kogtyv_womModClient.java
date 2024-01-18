9ee575672bb475a5f4aee7f8793c274e9ac4c8c7
kogtyv <kogtyv@gmail.com>
2024-01-17 22:08:34 -0500

    SetUp Gradle

---
+++ src/client/java/com/kogtyv_wom/kogtyv_womModClient.java
@@ -0,0 +1,10 @@
+package com.kogtyv_wom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class kogtyv_womModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
