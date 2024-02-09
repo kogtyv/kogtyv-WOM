a8abc091900a8f96003686786726eb64d3517a30
kogtyv <kogtyv@gmail.com>
2024-02-08 23:18:55 -0500

    Update files (code mod for Fabric)

---
+++ src/client/java/com/kogtyv/wom/WomModClient.java
@@ -0,0 +1,10 @@
+package com.kogtyv.wom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WomModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
